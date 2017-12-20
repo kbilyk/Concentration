443ff7977bdcd5c3b76bb7b533cd03ee98696474
Kostyantyn Bilyk <kos.bilyk@gmail.com>
2017-12-20 09:12:49 +0200

    Completed protocols part.

--- Concentrarion/ViewController.swift
+++ Concentrarion/ViewController.swift
@@ -51,14 +51,14 @@ class ViewController: UIViewController {
 
     private var emojiChoices = ["🦇","😱","🙀","😈","🎃","👻","🍭","🍬","🍎"]
 
-    private var emoji = [Int:String]()
+    private var emoji = [Card:String]()
 
     private func emoji(for card: Card) -> String {
-        if emojiChoices.count > 0 , emoji[card.identifier] == nil {
-            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+        if emojiChoices.count > 0 , emoji[card] == nil {
+            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
         }
 
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
     }
 }
 
