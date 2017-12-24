c4c97fce8270f06cd004d363b82ca1aa72aeeb07
Kostyantyn Bilyk <kos.bilyk@gmail.com>
2017-12-24 21:32:46 +0200

    Compleated Lecture#4.

--- Concentrarion/Concentration.swift
+++ Concentrarion/Concentration.swift
@@ -14,17 +14,7 @@ struct Concentration {
 
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
         }
         set {
             for index in cards.indices {
@@ -60,4 +50,11 @@ struct Concentration {
             cards.append(cards.remove(at: (cards.count - index).arc4random))
         }
     }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
+    }
+
 }
--- Concentrarion/ViewController.swift
+++ Concentrarion/ViewController.swift
@@ -18,11 +18,26 @@ class ViewController: UIViewController {
 
     private(set) var flipCount = 0 {
         didSet {
-            flipCountLabel.text = "Flips: \(flipCount)"
+            updateFlipCountLabel()
         }
     }
 
-    @IBOutlet private weak var flipCountLabel: UILabel!
+    private func updateFlipCountLabel() {
+        let attributes: [NSAttributedStringKey:Any] = [
+            .strokeWidth: 0.5,
+            .strokeColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        ]
+        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+
+        flipCountLabel.attributedText = attributedString
+    }
+
+    @IBOutlet private weak var flipCountLabel: UILabel! {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
+    
     @IBOutlet private var cardButtons: [UIButton]!
 
     @IBAction private func touchCardAction(_ sender: UIButton) {
@@ -49,13 +64,14 @@ class ViewController: UIViewController {
         }
     }
 
-    private var emojiChoices = ["🦇","😱","🙀","😈","🎃","👻","🍭","🍬","🍎"]
+    private var emojiChoices = "🦇😱🙀😈🎃👻🍭🍬🍎"
 
     private var emoji = [Card:String]()
 
     private func emoji(for card: Card) -> String {
         if emojiChoices.count > 0 , emoji[card] == nil {
-            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
 
         return emoji[card] ?? "?"
